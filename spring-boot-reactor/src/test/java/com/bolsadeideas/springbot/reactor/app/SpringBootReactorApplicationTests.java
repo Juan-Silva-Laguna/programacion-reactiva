a094bc7fd8e88f9c93fcbdb5fafd8027ff1eaab7
juan <juancho29silva@gmail.com>
2025-10-22 22:06:06 -0500

    Ajustes realizados en Mac

---
+++ spring-boot-reactor/src/test/java/com/bolsadeideas/springbot/reactor/app/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springbot.reactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
